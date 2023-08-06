85896001c55a1188281b51b1d1cb255cba18abb1
Hiteshini <hiteshinirithika@gmail.com>
2023-08-06 16:48:34 +0530

    Implement calculator application

--- server.py
+++ server.py
@@ -1,19 +1,37 @@
-from flask import Flask
+from flask import Flask,request, jsonify
 
 app = Flask(__name__)
 
-
-@app.route("/calculator/greeting", methods=['GET'])
+# Endpoint: GET /calculator/greeting
+@app.route('/calculator/greeting', methods=['GET'])
 def greeting():
-    return ''
+    return jsonify(message='Hello world!'), 200
 
-@app.route("/calculator/add", methods=['POST'])
+# Endpoint: POST /calculator/add
+@app.route('/calculator/add', methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    first = data.get('first', None)
+    second = data.get('second', None)
 
-@app.route("/calculator/subtract", methods=['POST'])
+    if first is None or second is None or not isinstance(first, (int, float)) or not isinstance(second, (int, float)):
+        return jsonify(error='Invalid input. Both first and second must be numbers.'), 400
+
+    result = first + second
+    return jsonify(result=result), 200
+
+# Endpoint: POST /calculator/subtract
+@app.route('/calculator/subtract', methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    first = data.get('first', None)
+    second = data.get('second', None)
+
+    if first is None or second is None or not isinstance(first, (int, float)) or not isinstance(second, (int, float)):
+        return jsonify(error='Invalid input. Both first and second must be numbers.'), 400
+
+    result = first - second
+    return jsonify(result=result), 200
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(debug=True)
